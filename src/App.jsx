794feb4447b199dffc3cb7e17fbfa613fe50108d
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-03-29 13:30:20 +0530

    dialog
    Co-authored-by: Vijay Makkad  <VijayMakkad@users.noreply.github.com>
    Co-authored-by: Arvind Pandey <arvindpandey4@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   const [url, setUrl] = useState('https://github.com/greeenboi/mobile_Chatapp'); // Default URL
   const [message, setMessage] = useState('');
-
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -32,49 +32,27 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <button type="button" onClick={() => setIsDialogOpen(true)}>
+        Clone Repository
+      </button>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <form onSubmit={handleSubmit} className="col">
-      <label htmlFor="url">Git Repository URL:</label>
-      <input
-        type="text"
-        id="url"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <button type="submit">Clone Repository</button>
+      <dialog open={isDialogOpen}>
+        <form onSubmit={handleSubmit} className="col">
+          <label htmlFor="url">Git Repository URL:</label>
+          <input
+            type="text"
+            id="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button type="submit">Clone</button>
+          <button type="button" onClick={() => setIsDialogOpen(false)}>
+            Close
+          </button>
+        </form>
+      </dialog>
       <p>{message}</p>
-    </form>
-
       <p>{greetMsg}</p>
     </div>
   );
